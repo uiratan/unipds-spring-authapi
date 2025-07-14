628ff3735028cc345f0eeca10d5e6699677030af
Uiratan Cavalcante <uiratan@gmail.com>
2025-07-14 16:01:01 -0300

    jwt token authentication

--- src/main/java/com/dev/uira/authapi/security/TokenUtil.java
+++ src/main/java/com/dev/uira/authapi/security/TokenUtil.java
@@ -2,14 +2,15 @@ package com.dev.uira.authapi.security;
 
 import com.dev.uira.authapi.controller.MyToken;
 import com.dev.uira.authapi.model.User;
-import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Collections;
 import java.util.Date;
@@ -17,7 +18,7 @@ import java.util.Date;
 public class TokenUtil {
 
     private static final String EMITER = "UiratanCavalcante";
-    private static final long EXPIRATION_TIME = 60 * 60 * 1000;
+    private static final long EXPIRATION_TIME = 15 * 1000;
     private static final String SECRET_KEY = "0123456789012345678901234567890123456789";
 
     public static MyToken encode(User user) {
@@ -38,20 +39,28 @@ public class TokenUtil {
     }
 
     public static Authentication decode(HttpServletRequest request) {
-        String header = request.getHeader("Authorization");
-        System.out.println("header: " + header);
-        if (header != null && isValidToken(header)) {
-            return new UsernamePasswordAuthenticationToken("valido", null, Collections.emptyList());
+        try {
+            String header = request.getHeader("Authorization");
+            System.out.println("header: " + header);
+            if (header != null) {
+                String token = header.replace("Bearer ", "");
+                SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+                JwtParser parser = Jwts.parser().verifyWith(key).build();
+                Claims claims = parser.parseClaimsJws(token).getBody();
+
+                String subject = claims.getSubject();
+                String issuer = claims.getIssuer();
+                Date expiration = claims.getExpiration();
+
+                if (issuer.equals(EMITER) && subject.length() > 0 && expiration.after(new Date(System.currentTimeMillis()))) {
+                    return new UsernamePasswordAuthenticationToken("valido", null, Collections.emptyList());
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    private static boolean isValidToken(String header) {
-        String token = getHeaderToken(header);
-        return token.equals("security123");
-    }
-
-    private static String getHeaderToken(String header) {
-        return header.replace("Bearer ", "");
-    }
 }
